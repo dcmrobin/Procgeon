116c4d1087981a2b694554878bc39d0d0671e319
DavidCM <dcm.robin@gmail.com>
2025-01-27 12:21:12 +0000

    got the sprites in a different file

---
+++ Main/Sprites.h
@@ -0,0 +1,37 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include <avr/pgmspace.h> // For PROGMEM
+
+// Stair sprite
+extern const unsigned char stairsSprite[];
+
+// Wall sprite
+extern const unsigned char wallSprite[];
+
+// Bars sprite
+extern const unsigned char barsSprite[];
+
+// Player sprites
+extern const unsigned char playerSpriteRight[];
+extern const unsigned char playerSpriteLeft[];
+extern const unsigned char* playerSprite;
+
+// Damsel sprites
+extern const unsigned char damselSpriteRight[];
+extern const unsigned char damselSpriteLeft[];
+extern const unsigned char damselHopefullSpriteRight[];
+extern const unsigned char damselHopefullSpriteLeft[];
+extern const unsigned char damselSpriteDead[];
+extern const unsigned char* damselSprite;
+
+// Blob sprites
+extern const unsigned char blobSpriteFrame1[];
+extern const unsigned char blobSpriteFrame2[];
+extern const unsigned char* blobSprite;
+
+// Screens
+extern const unsigned char rescueDamselScreen[];
+extern const unsigned char deadDamselScreen[];
+
+#endif // SPRITES_H
