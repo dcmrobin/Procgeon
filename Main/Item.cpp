5f7de0981e6a1c7dec9bcd1207e192909f2ea527
DavidCM <dcm.robin@gmail.com>
2025-06-30 15:09:58 +0100

    added grey potion and NoEffect potion effect

--- Main/Item.cpp
+++ Main/Item.cpp
@@ -18,6 +18,7 @@ GameItem itemList[] = {
   { MaroonPotion, PotionCategory, String("Maroon Potion"), 0,  0,  0,  0, 0, String("Drink it to find out."), String("Maroon Potion"), String("Nothing happens.")},
   { DarkGreenPotion, PotionCategory, String("DarkGreen Potion"), 0,  0,  0,  0, 0, String("Drink it to find out."), String("DarkGreen Potion"), String("Nothing happens.")},
   { LimePotion, PotionCategory, String("Lime Potion"), 0,  0,  0,  0, 0, String("Drink it to find out."), String("Lime Potion"), String("Nothing happens.")},
+  { GreyPotion, PotionCategory, String("Grey Potion"), 0,  0,  0,  0, 0, String("Drink it to find out."), String("Grey Potion"), String("Nothing happens.")},
   { Mushroom, FoodCategory, String("Mushroom"), 0,  20,  0, 0, 0, String("It is edible."), String("Mushroom"), String("You become less hungry.")},
   { EmptyBottle, PotionCategory, String("Empty Bottle"), 0,  20,  0, 0, 0, String("It is an empty bottle."), String("Empty Bottle"), String("Nothing happens. It's an empty bottle."), false},
   { RiddleStone, EquipmentCategory, String("Riddle Stone"), 0,  0,  0, 0, 0, String("Looks like it could be used for many things..."), String("Riddle Stone"), String("Solve this riddle!"), true, DefaultEffect, 0, false, false, 2},
@@ -50,7 +51,8 @@ PotionEffect potionEffects[] = {
   { 0,  0,  0, 0, String("Hunger Potion"), String("Makes you more hungry."), String("You are now more hungry."), HungerEffect },
   { 0,  0,  0, 0, String("See-all Potion"), String("Opens your eyes to the unseen."), String("You can now see that which was unseen for a limited time."), SeeAllEffect },
   { 0,  0,  0, 0, String("Confusion Potion"), String("You go in the opposite direction to the direction you want to go."), String("What is going on?"), ConfusionEffect },
-  { 0,  0,  0, 0, String("Ridicule Potion"), String("Drinking this makes you feel stupid."), String("You feel stupid."), RidiculeEffect }
+  { 0,  0,  0, 0, String("Ridicule Potion"), String("Drinking this makes you feel stupid."), String("You feel stupid."), RidiculeEffect },
+  { 0,  0,  0, 0, String("Bland Potion"), String("Colored liquid that does nothing."), String("Nothing happens."), NoEffect }
 };
 
 ScrollEffect scrollEffects[] = {
@@ -68,7 +70,8 @@ ItemCombination itemCombinations[] = {
     {GreenPotion, BluePotion, CyanPotion},
     {RedPotion, BlackPotion, MaroonPotion},
     {GreenPotion, BlackPotion, DarkGreenPotion},
-    {GreenPotion, YellowPotion, LimePotion}
+    {GreenPotion, YellowPotion, LimePotion},
+    {WhitePotion, BlackPotion, GreyPotion}
     // Add more general item combinations here
 };
 
