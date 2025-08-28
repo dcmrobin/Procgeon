f12684ddc2115d2d786373dfe6ae5d0e7adf5f62
DavidCM <dcm.robin@gmail.com>
2025-08-28 17:36:08 +0100

    now certain enemies don't spawn until later dungeons

--- Main/Dungeon.cpp
+++ Main/Dungeon.cpp
@@ -268,13 +268,13 @@ void spawnEnemies() {
       int ex = random(0, mapWidth);
       int ey = random(0, mapHeight);
       if (dungeonMap[ey][ex] == Floor && sqrt(pow(playerX - ex, 2) + pow(playerY - ey, 2)) >= 10) {
-        if (random(0, 5) == 1) {
+        if (random(0, 5) == 1 && dungeon > 1) {
           enemies[i] = { (float)ex, (float)ey, 20, false, 0.05, "blob", 20, 5, false, 0, 0 };
           enemies[i].sprite = blobAnimation[random(0, blobAnimationLength)].frame;
-        } else if (random(0, 4) == 2) {
+        } else if (random(0, 4) == 2 && dungeon > 2) {
           enemies[i] = { (float)ex, (float)ey, 10, false, 0.11, "teleporter", 20, 0, false, 0, 0 };
           enemies[i].sprite = teleporterAnimation[random(0, teleporterAnimationLength)].frame;
-        } else if (random(0, 6) == 4) {
+        } else if (random(0, 6) == 4 && dungeon > 4) {
           enemies[i] = { (float)ex, (float)ey, 15, false, 0.06, "shooter", 20, 0, false, 0, 0 };
           enemies[i].sprite = shooterAnimation[random(0, shooterAnimationLength)].frame;
         } else {
